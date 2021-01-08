641ba91ae83817c6508c0c5318c2af4a06c1cdc2
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2021-01-08 06:01:11 -0800

    clarify script wrapper constraints

--- CommonLibF4/include/RE/Bethesda/BSScriptUtil.h
+++ CommonLibF4/include/RE/Bethesda/BSScriptUtil.h
@@ -35,12 +35,12 @@ namespace RE::BSScript
 	}
 
 	template <
-		stl::nttp::string O,
-		stl::nttp::string S>
+		stl::nttp::string Object,
+		stl::nttp::string Structure>
 	class structure_wrapper
 	{
 	private:
-		static constexpr stl::nttp::string _full = detail::make_structure_tag(O, S);
+		static constexpr stl::nttp::string _full = detail::make_structure_tag(Object, Structure);
 
 	public:
 		static constexpr std::string_view name{ _full.data(), _full.length() };
@@ -171,7 +171,7 @@ namespace RE::BSScript
 		{};
 
 		template <class T>
-		using is_structure_wrapper = _is_structure_wrapper<std::decay_t<T>>;
+		using is_structure_wrapper = _is_structure_wrapper<std::remove_cv_t<T>>;
 
 		template <class T>
 		inline constexpr bool is_structure_wrapper_v = is_structure_wrapper<T>::value;
@@ -202,15 +202,15 @@ namespace RE::BSScript
 		// clang-format on
 
 		template <class T>
-		concept _string =
+		concept string =
 			std::same_as<
-				typename script_traits<T>::is_string,
+				typename script_traits<std::remove_cv_t<T>>::is_string,
 				std::true_type> &&
 			std::convertible_to<T, std::string_view> &&
 			std::constructible_from<T, std::string_view>;
 
 		template <class T>
-		concept string = _string<std::decay_t<T>>;
+		concept string_ref = string<std::decay_t<T>>;
 
 		template <class T>
 		concept integral =
@@ -237,9 +237,9 @@ namespace RE::BSScript
 
 		// clang-format off
 		template <class T>
-		concept _array =
+		concept array =
 			std::same_as<
-				typename script_traits<T>::is_array,
+				typename script_traits<std::remove_cv_t<T>>::is_array,
 				std::true_type> &&
 			std::is_default_constructible_v<T> &&
 			requires(T a_array, typename T::value_type a_value)
@@ -247,31 +247,42 @@ namespace RE::BSScript
 			{ a_array.begin() } -> std::same_as<typename T::iterator>;
 			{ a_array.end() } -> std::same_as<typename T::iterator>;
 			{ a_array.size() } -> std::same_as<typename T::size_type>;
-			a_array.push_back(std::move(a_value));
+			a_array.push_back(static_cast<typename T::value_type&&>(a_value));
 		};
 		// clang-format on
 
 		template <class T>
-		concept array = _array<std::decay_t<T>>;
+		concept array_ref = array<std::decay_t<T>>;
 
 		template <class T>
 		concept wrapper = is_structure_wrapper_v<T>;
 
 		template <class T>
-		concept _nullable =
-			std::same_as<
-				typename script_traits<T>::is_nullable,
-				std::true_type> &&
-			(array<typename T::value_type> ||
-				wrapper<typename T::value_type>);
+		concept wrapper_ref = wrapper<std::decay_t<T>>;
 
 		template <class T>
-		concept nullable = _nullable<std::decay_t<T>>;
+		concept nullable =
+			std::same_as<
+				typename script_traits<std::remove_cv_t<T>>::is_nullable,
+				std::true_type> &&
+			std::is_default_constructible_v<T> &&
+			(array<typename T::value_type> ||
+				wrapper<typename T::value_type>)&&  //
+			requires(T a_nullable)
+		{
+			// clang-format off
+			static_cast<bool>(a_nullable);
+			{ *static_cast<T&&>(a_nullable) } -> std::convertible_to<typename T::value_type>;
+			// clang-format on
+		};
+
+		template <class T>
+		concept nullable_ref = nullable<std::decay_t<T>>;
 
 		template <class T>
 		concept valid_self =
 			static_tag<T> ||
-			(std::is_reference_v<T>&& object<decay_t<T>>);
+			(std::is_lvalue_reference_v<T>&& object<decay_t<T>>);
 
 		template <class T>
 		concept valid_parameter =
@@ -299,7 +310,7 @@ namespace RE::BSScript
 				return T(get<Struct>(a_var));
 			}
 
-			template <wrapper T>
+			template <wrapper_ref T>
 			[[nodiscard]] static auto get_proxy(T&& a_wrapper)
 			{
 				return std::forward<T>(a_wrapper).get_proxy();
@@ -458,7 +469,7 @@ namespace RE::BSScript
 		}
 	}
 
-	template <detail::string T>
+	template <detail::string_ref T>
 	void PackVariable(Variable& a_var, T&& a_val)
 	{
 		a_var = BSFixedString(std::forward<T>(a_val));
@@ -488,7 +499,7 @@ namespace RE::BSScript
 		a_var = static_cast<bool>(a_val);
 	}
 
-	template <detail::array T>
+	template <detail::array_ref T>
 	void PackVariable(Variable& a_var, T&& a_val)
 	{
 		using size_type = typename detail::decay_t<T>::size_type;
@@ -497,7 +508,7 @@ namespace RE::BSScript
 		const auto success = [&]() {
 			const auto game = GameVM::GetSingleton();
 			const auto vm = game ? game->GetVM() : nullptr;
-			const auto typeInfo = GetTypeInfo<T>();
+			const auto typeInfo = GetTypeInfo<std::decay_t<T>>();
 			const auto size = a_val.size();
 			BSTSmartPointer<Array> out;
 			if (!typeInfo ||
@@ -523,17 +534,17 @@ namespace RE::BSScript
 		}
 	}
 
-	template <detail::wrapper T>
+	template <detail::wrapper_ref T>
 	void PackVariable(Variable& a_var, T&& a_val)
 	{
 		a_var = detail::wrapper_accessor::get_proxy(std::forward<T>(a_val));
 	}
 
-	template <detail::nullable T>
+	template <detail::nullable_ref T>
 	void PackVariable(Variable& a_var, T&& a_val)
 	{
-		if (a_val.has_value()) {
-			PackVariable(a_var, std::forward<T>(a_val).value());
+		if (a_val) {
+			PackVariable(a_var, *std::forward<T>(a_val));
 		} else {
 			a_var = nullptr;
 		}
@@ -637,10 +648,10 @@ namespace RE::BSScript
 	[[nodiscard]] T UnpackVariable(const Variable& a_var)
 	{
 		if (a_var.is<std::nullptr_t>()) {
-			return {};
+			return T();
 		} else {
 			using value_type = typename std::decay_t<T>::value_type;
-			return UnpackVariable<value_type>(a_var);
+			return T(UnpackVariable<value_type>(a_var));
 		}
 	}
 
@@ -650,6 +661,71 @@ namespace RE::BSScript
 		[[nodiscard]] __forceinline T UnpackVariable(const Variable& a_var)
 		{
 			return BSScript::UnpackVariable<T>(a_var);
+		}
+
+		template <class T>
+		consteval void ValidateParameter() noexcept
+		{
+			// Must be one of (optionally cv-qualified):
+			//	* A pointer to (optionally cv-qualified) (optionally derived from) `RE::TESForm`
+			//	* A string type which is one of:
+			//		* std::string
+			//		* std::string_view
+			//		* RE::BSFixedString
+			//		* RE::BSFixedStringCS
+			//		* A custom type which implements:
+			//			* A specialization of `RE::BSScript::script_traits` which provides a typedef `is_string` as `std::true_type`
+			//			* A conversion to `std::string_view`
+			//	* An integral type which is one of (optionally signed/unsigned):
+			//		* `char`
+			//		* `short`
+			//		* `int`
+			//		* `long`
+			//		* `long long`
+			//	* An enumeration type
+			//	* A floating point type which is one of:
+			//		* `float`
+			//		* `double`
+			//		* `long double`
+			//	* `bool`
+			//	* An array type which is one of:
+			//		* std::vector
+			//		* A custom type which implements:
+			//			* A specialization of `RE::BSScript::script_traits` which provides a typedef `is_array` as `std::true_type`
+			// 			* A default constructor
+			//			* The following typedefs:
+			//				* `iterator`
+			//				* `size_type`
+			//				* `value_type`
+			//			* The following methods:
+			//				* `begin`
+			//					* Callable with no arguments
+			//					* Returns a type of `iterator`
+			//				* `end`
+			//					* Callable with no arguments
+			//					* Returns a type of `iterator`
+			//				* `size`
+			//					* Callable with no arguments
+			//					* Returns a type of `size_type`
+			//				* `push_back`
+			//					* Callable with an rvalue reference of type `value_type`
+			//	* A wrapper type which is one of:
+			//		* `RE::BSScript::structure_wrapper`
+			//	* A nullable type which is one of:
+			//		* std::optional
+			//		* A custom type which implements:
+			//			* A specialization of `RE::BSScript::script_traits` which provides a typedef `is_nullable` as `std::true_type`
+			//			* A default constructor
+			//			* The following typedefs:
+			//				* `value_type`
+			//			* The following methods:
+			//				* operator bool
+			//				* operator *
+			//					* Returns type of `value_type`
+			//		* Additionally, the nullable's `value_type` must be one of:
+			//			* An array type
+			//			* A structure type
+			static_assert(detail::valid_parameter<T>, "invalid parameter type");
 		}
 
 		template <
@@ -700,9 +776,9 @@ namespace RE::BSScript
 	template <
 		class F,
 		bool LONG,
-		detail::valid_return R,
-		detail::valid_self S,
-		detail::valid_parameter... Args>
+		class R,
+		class S,
+		class... Args>
 	class NativeFunction :
 		public NF_util::NativeFunctionBase
 	{
@@ -710,6 +786,18 @@ namespace RE::BSScript
 		using super = NF_util::NativeFunctionBase;
 
 	public:
+		// Must be one of:
+		//	* A valid parameter type
+		//	* `void`
+		static_assert(detail::valid_return<R>, "invalid return type");
+
+		// A function which takes a self parameter must be one of:
+		//	* An lvalue reference to (optionally cv-qualified) (optionally derived from) `RE::TESForm`
+		// A function which does not take a self parameter (a global function) must tag the self slot with `std::monostate`
+		static_assert(detail::valid_self<S>, "invalid self type");
+
+		static_assert(((detail::ValidateParameter<Args>(), ...), true));
+
 		template <class Fn>
 		NativeFunction(std::string_view a_object, std::string_view a_function, Fn a_func)  //
 			requires(detail::invocable_r<Fn, R, S, Args...> ||
@@ -762,9 +850,9 @@ namespace RE::BSScript
 	};
 
 	template <
-		detail::valid_return R,
-		detail::valid_self S,
-		detail::valid_parameter... Args>
+		class R,
+		class S,
+		class... Args>
 	NativeFunction(std::string_view, std::string_view, R (*)(S, Args...))
 		-> NativeFunction<
 			R(S, Args...),
@@ -774,9 +862,9 @@ namespace RE::BSScript
 			Args...>;
 
 	template <
-		detail::valid_return R,
-		detail::valid_self S,
-		detail::valid_parameter... Args>
+		class R,
+		class S,
+		class... Args>
 	NativeFunction(std::string_view, std::string_view, R (*)(IVirtualMachine&, std::uint32_t, S, Args...))
 		-> NativeFunction<
 			R(IVirtualMachine&, std::uint32_t, S, Args...),
