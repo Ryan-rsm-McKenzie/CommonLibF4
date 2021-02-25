b2fe0fc4231a6c016755a46a2166cccc66e254ff
Qudix <17361645+Qudix@users.noreply.github.com>
2021-02-25 13:48:49 -0600

    Scaleform

--- CommonLibF4/include/RE/Scaleform/GFx/GFx_Player.cpp
+++ CommonLibF4/include/RE/Scaleform/GFx/GFx_Player.cpp
@@ -6,7 +6,7 @@ namespace RE::Scaleform::GFx
 {
 	void Movie::CreateFunction(Value* a_value, FunctionHandler* a_function, void* a_userData)
 	{
-		return asMovieRoot->CreateFunction(a_value, a_function, a_userData);
+		asMovieRoot->CreateFunction(a_value, a_function, a_userData);
 	}
 
 	bool Movie::GetVariable(Value* a_val, const char* a_pathToVar) const
