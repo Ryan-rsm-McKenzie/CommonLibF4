83583d32e7b6a5686b966a89785c53643018a564
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2021-06-17 15:22:53 -0700

    simplify post-build copy

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,9 +1,15 @@
 cmake_minimum_required(VERSION 3.20)
 
+macro(set_from_environment VARIABLE)
+	if(NOT DEFINED ${VARIABLE} AND DEFINED ENV{${VARIABLE}})
+		set(${VARIABLE} $ENV{${VARIABLE}})
+	endif()
+endmacro()
+
 macro(add_project)
 	set(_PREFIX add_project)
 
-	set(_OPTIONS PLACEHOLDER)
+	set(_OPTIONS)
 	set(_ONE_VALUE_ARGS
 		LIBRARY_TYPE
 		PROJECT
@@ -33,8 +39,22 @@ macro(add_project)
 
 	foreach(_ARG ${_REQUIRED})
 		if(NOT DEFINED "${_PREFIX}_${_ARG}")
-			message(FATAL_ERROR "Argument ${_ARG} is required to be defined")
+			message(FATAL_ERROR "Argument is required to be defined: ${_ARG}")
 		endif()
+	endforeach()
+
+	set(_CLEANUP
+		_PREFIX
+		_OPTIONS
+		_ONE_VALUE_ARGS
+		_MULTI_VALUE_ARGS
+		_REQUIRED
+		${_PREFIX}_UNPARSED_ARGUMENTS
+		${_PREFIX}_KEYWORDS_MISSING_VALUES
+	)
+
+	foreach(_ARG IN LISTS _OPTIONS _ONE_VALUE_ARGS _MULTI_VALUE_ARGS)
+		list(APPEND _CLEANUP "${_PREFIX}_${_ARG}")
 	endforeach()
 
 	# ---- Argument validation
@@ -48,17 +68,17 @@ macro(add_project)
 	endif()
 
 	set(_TARGET_TYPES executable library)
+	list(APPEND _CLEANUP _TARGET_TYPES)
 	if(NOT "${${_PREFIX}_TARGET_TYPE}" IN_LIST _TARGET_TYPES)
 		message(FATAL_ERROR "TARGET_TYPE \"${${_PREFIX}_TARGET_TYPE}\" must be one of: ${_TARGET_TYPES}")
 	endif()
-	unset(_TARGET_TYPES)
 
 	set(_LIBRARY_TYPES STATIC SHARED MODULE)
+	list(APPEND _CLEANUP _LIBRARY_TYPES)
 	if(DEFINED "${_PREFIX}_LIBRARY_TYPE" AND
 		NOT "${${_PREFIX}_LIBRARY_TYPE}" IN_LIST _LIBRARY_TYPES)
 		message(FATAL_ERROR "LIBRARY_TYPE \"${${_PREFIX}_LIBRARY_TYPE}\" must be one of: ${_LIBRARY_TYPES}")
 	endif()
-	unset(_LIBRARY_TYPES)
 
 	if("${_PREFIX}_TARGET_TYPE" STREQUAL "library" AND
 		NOT DEFINED "${_PREFIX}_LIBRARY_TYPE")
@@ -72,6 +92,9 @@ macro(add_project)
 
 	if(DEFINED "${_PREFIX}_VERSION")
 		set(_VERSION_PREFIX "VERSION")
+		list(APPEND _CLEANUP _VERSION_PREFIX)
+	else()
+		unset(_VERSION_PREFIX)
 	endif()
 
 	project(
@@ -79,8 +102,6 @@ macro(add_project)
 		${_VERSION_PREFIX} ${${_PREFIX}_VERSION}
 		LANGUAGES CXX
 	)
-
-	unset(_VERSION_PREFIX)
 
 	# ---- Include guards ----
 
@@ -194,13 +215,54 @@ macro(add_project)
 
 	# ---- Cleanup local variables ----
 
-	foreach(_ARG IN LISTS _OPTIONS _ONE_VALUE_ARGS _MULTI_VALUE_ARGS)
-		unset("${_PREFIX}_${_ARG}")
+	foreach(_VAR "${_CLEANUP}")
+		unset("${_VAR}")
 	endforeach()
+	unset(_CLEANUP)
+endmacro()
 
-	unset(_OPTIONS)
-	unset(_ONE_VALUE_ARGS)
-	unset(_MULTI_VALUE_ARGS)
-	unset(_REQUIRED)
-	unset(_PREFIX)
+macro(copy_files)
+	if(NOT ${ARGC} GREATER 0)
+		message(FATAL_ERROR "Invalid number of arguments.")
+	endif()
+
+	math(EXPR _REMAINDER "${ARGC} % 2")
+	if(NOT _REMAINDER EQUAL 0)
+		message(FATAL_ERROR "Arguments must be paired as a file + path spec.")
+	endif()
+
+	option(COPY_BUILD "Copy the build output to the Fallout 4 directory." OFF)
+
+	set(_ARGS "${ARGN}")
+
+	if(COPY_BUILD)
+		set_from_environment(Fallout4Path)
+		if(DEFINED Fallout4Path)
+			math(EXPR _PAIRS "${ARGC} / 2 - 1")
+			foreach(_IDX RANGE ${_PAIRS})
+				math(EXPR _IDX "${_IDX} * 2")
+				math(EXPR _IDXN "${_IDX} + 1")
+
+				list(GET _ARGS ${_IDX} _FROM)
+				list(GET _ARGS ${_IDXN} _TO)
+
+				cmake_path(SET _TO NORMALIZE "${Fallout4Path}/${_TO}")
+
+				add_custom_command(
+					TARGET ${PROJECT_NAME}
+					POST_BUILD
+					COMMAND ${CMAKE_COMMAND} -E copy_if_different "${_FROM}" "${_TO}"
+				)
+			endforeach()
+		else()
+			message(WARNING "Variable Fallout4Path is not defined. Skipping post-build copy command.")
+		endif()
+	endif()
+
+	unset(_ARGS)
+	unset(_REMAINDER)
+	unset(_IDX)
+	unset(_IDXN)
+	unset(_FROM)
+	unset(_TO)
 endmacro()
