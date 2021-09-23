44f2b4035567c42135d0dd2cd09382a789dd5493
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2021-09-22 22:40:17 -0700

    fix NiTArray

--- CommonLibF4/include/RE/NetImmerse/NiTArray.h
+++ CommonLibF4/include/RE/NetImmerse/NiTArray.h
@@ -15,8 +15,117 @@ namespace RE
 		using const_reference = const value_type&;
 		using pointer = value_type*;
 		using const_pointer = const value_type*;
-		using iterator = value_type*;
-		using const_iterator = const value_type*;
+
+		template <class U>
+		class iterator_base :
+			public boost::stl_interfaces::iterator_interface<
+				iterator_base<U>,
+				std::bidirectional_iterator_tag,
+				U>
+		{
+		private:
+			using super =
+				boost::stl_interfaces::iterator_interface<
+					iterator_base<U>,
+					std::bidirectional_iterator_tag,
+					U>;
+
+		public:
+			using difference_type = typename super::difference_type;
+			using value_type = typename super::value_type;
+			using pointer = typename super::pointer;
+			using reference = typename super::reference;
+			using iterator_category = typename super::iterator_category;
+
+			~iterator_base() noexcept = default;
+			iterator_base() noexcept = default;
+			iterator_base(const volatile iterator_base&) = delete;
+			iterator_base& operator=(const volatile iterator_base&) = delete;
+
+			template <class V>
+			iterator_base(const iterator_base<V>& a_rhs)  //
+				requires(std::convertible_to<typename iterator_base<V>::reference, reference>) :
+				_pos(a_rhs._pos),
+				_head(a_rhs._head),
+				_tail(a_rhs._tail)
+			{}
+
+			template <class V>
+			iterator_base& operator=(const iterator_base<V>& a_rhs)  //
+				requires(std::convertible_to<typename iterator_base<V>::reference, reference>)
+			{
+				_pos = a_rhs._pos;
+				_head = a_rhs._head;
+				_tail = a_rhs._tail;
+				return *this;
+			}
+
+			[[nodiscard]] reference operator*() const noexcept
+			{
+				assert(validate());
+				return *_pos;
+			}
+
+			template <class V>
+			[[nodiscard]] bool operator==(const iterator_base<V>& a_rhs) const noexcept
+			{
+				assert(_head == a_rhs._head);
+				assert(_tail == a_rhs._tail);
+				return _pos == a_rhs._pos;
+			}
+
+			using super::operator++;
+
+			void operator++() noexcept
+			{
+				assert(validate());
+				do {
+					++_pos;
+				} while (_pos < _tail && !slot_filled());
+			}
+
+			using super::operator--;
+
+			void operator--() noexcept
+			{
+				assert(validate());
+				do {
+					--_pos;
+				} while (_head <= _pos && !slot_filled());
+			}
+
+		private:
+			friend iterator_base;
+			friend NiTArray;
+
+			explicit iterator_base(pointer a_pos, pointer a_head, pointer a_tail) noexcept :
+				_pos(a_pos),
+				_head(a_head),
+				_tail(a_tail)
+			{}
+
+			[[nodiscard]] bool validate() const noexcept
+			{
+				return _pos != nullptr &&
+				       _head != nullptr &&
+				       _tail != nullptr &&
+				       _head <= _pos && _pos < _tail;
+			}
+
+			[[nodiscard]] bool slot_filled() const noexcept
+			{
+				assert(validate());
+				return static_cast<bool>(*_pos);
+			}
+
+			pointer _pos{ nullptr };
+			pointer _head{ nullptr };
+			pointer _tail{ nullptr };
+		};
+
+	public:
+		using iterator = iterator_base<value_type>;
+		using const_iterator = iterator_base<const value_type>;
 
 		explicit NiTArray(std::uint32_t a_maxSize = 0, std::uint32_t a_growBy = 1) :
 			_capacity(static_cast<std::uint16_t>(a_maxSize)),
@@ -29,41 +138,39 @@ namespace RE
 
 		virtual ~NiTArray() { allocator_type::deallocate(_data); }  // 00
 
-		[[nodiscard]] reference operator[](size_type a_pos)
+		[[nodiscard]] reference operator[](size_type a_pos) noexcept
 		{
-			assert(a_pos < size());
+			assert(a_pos < capacity());
 			return _data[a_pos];
 		}
 
-		[[nodiscard]] const_reference operator[](size_type a_pos) const
+		[[nodiscard]] const_reference operator[](size_type a_pos) const noexcept
 		{
-			assert(a_pos < size());
+			assert(a_pos < capacity());
 			return _data[a_pos];
 		}
-
-		[[nodiscard]] reference front() { return operator[](0); }
-		[[nodiscard]] const_reference front() const { return operator[](0); }
-
-		[[nodiscard]] reference back() { return operator[](size() - 1); }
-		[[nodiscard]] const_reference back() const { return operator[](size() - 1); }
 
 		[[nodiscard]] const_pointer data() const noexcept { return _data; }
 
-		[[nodiscard]] iterator begin() { return _data; }
-		[[nodiscard]] const_iterator begin() const { return _data; }
-		[[nodiscard]] const_iterator cbegin() const { return _data; }
+		[[nodiscard]] iterator begin() noexcept { return make_iterator<iterator>(0); }
+		[[nodiscard]] const_iterator begin() const noexcept { return make_iterator<const_iterator>(0); }
+		[[nodiscard]] const_iterator cbegin() const noexcept { return make_iterator<const_iterator>(0); }
 
-		[[nodiscard]] iterator end() { return _data + _size; }
-		[[nodiscard]] const_iterator end() const { return _data + _size; }
-		[[nodiscard]] const_iterator cend() const noexcept { return _data + _size; }
+		[[nodiscard]] iterator end() noexcept { return make_iterator<iterator>(_capacity); }
+		[[nodiscard]] const_iterator end() const noexcept { return make_iterator<const_iterator>(_capacity); }
+		[[nodiscard]] const_iterator cend() const noexcept { return make_iterator<const_iterator>(_capacity); }
 
 		[[nodiscard]] bool empty() const noexcept { return size() == 0; }
-
 		[[nodiscard]] size_type size() const noexcept { return _size; }
-
 		[[nodiscard]] size_type capacity() const noexcept { return _capacity; }
 
 	private:
+		template <class T>
+		[[nodiscard]] T make_iterator(size_type a_pos) const noexcept
+		{
+			return T(_data + a_pos, _data, _data + _capacity);
+		}
+
 		// members
 		pointer _data{ nullptr };    // 08
 		size_type _capacity{ 0 };    // 10
