46c2a0dbe81e898be9118955802fd445f1ed7682
Hxef <hxef@users.noreply.github.com>
2022-10-17 23:50:19 +0300

    Fix calloc returning junk data

--- CommonLibF4/include/RE/Bethesda/MemoryManager.h
+++ CommonLibF4/include/RE/Bethesda/MemoryManager.h
@@ -289,7 +289,11 @@ namespace RE
 
 	[[nodiscard]] inline void* calloc(std::size_t a_num, std::size_t a_size)
 	{
-		return malloc(a_num * a_size);
+		const auto ret = malloc(a_num * a_size);
+		if (ret) {
+			std::memset(ret, 0, a_num * a_size);
+		}
+		return ret;
 	}
 
 	template <class T>
