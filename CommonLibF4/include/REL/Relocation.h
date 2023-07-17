854b643b8d3e391c2ba93b790d6b87a09e293025
Qudix <17361645+Qudix@users.noreply.github.com>
2023-07-17 03:12:25 -0500

    `REL` Version, make_pattern, misc

--- CommonLibF4/include/REL/Relocation.h
+++ CommonLibF4/include/REL/Relocation.h
@@ -164,6 +164,8 @@ namespace REL
 	}
 
 	inline constexpr std::uint8_t NOP = 0x90;
+	inline constexpr std::uint8_t JMP8 = 0xEB;
+	inline constexpr std::uint8_t JMP32 = 0xE9;
 	inline constexpr std::uint8_t RET = 0xC3;
 	inline constexpr std::uint8_t INT3 = 0xCC;
 
@@ -258,6 +260,32 @@ namespace REL
 			_impl{ a_v1, a_v2, a_v3, a_v4 }
 		{}
 
+		explicit constexpr Version(std::string_view a_version)
+		{
+			std::array<value_type, 4> powers{ 1, 1, 1, 1 };
+			std::size_t position = 0;
+			for (std::size_t i = 0; i < a_version.size(); ++i) {
+				if (a_version[i] == '.') {
+					if (++position == powers.size()) {
+						throw std::invalid_argument("Too many parts in version number.");
+					}
+				} else {
+					powers[position] *= 10;
+				}
+			}
+			position = 0;
+			for (std::size_t i = 0; i < a_version.size(); ++i) {
+				if (a_version[i] == '.') {
+					++position;
+				} else if (a_version[i] < '0' || a_version[i] > '9') {
+					throw std::invalid_argument("Invalid character in version number.");
+				} else {
+					powers[position] /= 10;
+					_impl[position] += static_cast<value_type>((a_version[i] - '0') * powers[position]);
+				}
+			}
+		}
+
 		[[nodiscard]] constexpr reference operator[](std::size_t a_idx) noexcept { return _impl[a_idx]; }
 		[[nodiscard]] constexpr const_reference operator[](std::size_t a_idx) const noexcept { return _impl[a_idx]; }
 
@@ -276,26 +304,50 @@ namespace REL
 			return std::strong_ordering::equal;
 		}
 
-		[[nodiscard]] std::string string() const
+		[[nodiscard]] constexpr std::uint32_t pack() const noexcept
+		{
+			return static_cast<std::uint32_t>(
+				(_impl[0] & 0x0FF) << 24u |
+				(_impl[1] & 0x0FF) << 16u |
+				(_impl[2] & 0xFFF) << 4u |
+				(_impl[3] & 0x00F) << 0u);
+		}
+
+		[[nodiscard]] constexpr value_type major() const noexcept { return _impl[0]; }
+		[[nodiscard]] constexpr value_type minor() const noexcept { return _impl[1]; }
+		[[nodiscard]] constexpr value_type patch() const noexcept { return _impl[2]; }
+		[[nodiscard]] constexpr value_type build() const noexcept { return _impl[3]; }
+
+		[[nodiscard]] std::string string(std::string_view a_separator = "."sv) const
 		{
 			std::string result;
 			for (auto&& ver : _impl) {
 				result += std::to_string(ver);
-				result += '-';
+				result += a_separator;
 			}
 			result.pop_back();
 			return result;
 		}
 
-		[[nodiscard]] std::wstring wstring() const
+		[[nodiscard]] std::wstring wstring(std::wstring_view a_separator = L"."sv) const
 		{
 			std::wstring result;
 			for (auto&& ver : _impl) {
 				result += std::to_wstring(ver);
-				result += L'-';
+				result += a_separator;
 			}
 			result.pop_back();
 			return result;
+		}
+
+		[[nodiscard]] static constexpr Version unpack(std::uint32_t a_packedVersion) noexcept
+		{
+			return REL::Version{
+				static_cast<value_type>((a_packedVersion >> 24) & 0x0FF),
+				static_cast<value_type>((a_packedVersion >> 16) & 0x0FF),
+				static_cast<value_type>((a_packedVersion >> 4) & 0xFFF),
+				static_cast<value_type>(a_packedVersion & 0x0F)
+			};
 		}
 
 	private:
@@ -304,6 +356,50 @@ namespace REL
 
 	[[nodiscard]] constexpr bool operator==(const Version& a_lhs, const Version& a_rhs) noexcept { return a_lhs.compare(a_rhs) == 0; }
 	[[nodiscard]] constexpr std::strong_ordering operator<=>(const Version& a_lhs, const Version& a_rhs) noexcept { return a_lhs.compare(a_rhs); }
+
+	namespace literals
+	{
+		namespace detail
+		{
+			template <std::size_t Index, char C>
+			constexpr uint8_t read_version(std::array<typename REL::Version::value_type, 4>& result)
+			{
+				static_assert(C >= '0' && C <= '9', "Invalid character in semantic version literal.");
+				static_assert(Index < 4, "Too many components in semantic version literal.");
+				result[Index] += (C - '0');
+				return 10;
+			}
+
+			template <std::size_t Index, char C, char... Rest>
+				requires(sizeof...(Rest) > 0)
+			constexpr uint8_t read_version(std::array<typename REL::Version::value_type, 4>& result)
+			{
+				static_assert(C == '.' || (C >= '0' && C <= '9'), "Invalid character in semantic version literal.");
+				static_assert(Index < 4, "Too many components in semantic version literal.");
+				if constexpr (C == '.') {
+					read_version<Index + 1, Rest...>(result);
+					return 1;
+				} else {
+					auto position = read_version<Index, Rest...>(result);
+					result[Index] += (C - '0') * position;
+					return position * 10;
+				}
+			}
+		}
+
+		template <char... C>
+		[[nodiscard]] constexpr REL::Version operator""_v() noexcept
+		{
+			std::array<typename REL::Version::value_type, 4> result{ 0, 0, 0, 0 };
+			detail::read_version<0, C...>(result);
+			return REL::Version(result);
+		}
+
+		[[nodiscard]] constexpr REL::Version operator""_v(const char* str, std::size_t len)
+		{
+			return Version(std::string_view(str, len));
+		}
+	}
 
 	[[nodiscard]] inline std::optional<Version> get_file_version(stl::zwstring a_filename)
 	{
@@ -686,6 +782,10 @@ namespace REL
 			_impl{ a_id.address() + a_offset }
 		{}
 
+		explicit Relocation(ID a_id, Offset a_offset) :
+			_impl{ a_id.address() + a_offset.offset() }
+		{}
+
 		constexpr Relocation& operator=(std::uintptr_t a_address) noexcept
 		{
 			_impl = a_address;
@@ -759,6 +859,216 @@ namespace REL
 		// clang-format on
 
 		std::uintptr_t _impl{ 0 };
+	};
+
+	namespace detail
+	{
+		namespace characters
+		{
+			[[nodiscard]] constexpr bool hexadecimal(char a_ch) noexcept
+			{
+				return ('0' <= a_ch && a_ch <= '9') ||
+				       ('A' <= a_ch && a_ch <= 'F') ||
+				       ('a' <= a_ch && a_ch <= 'f');
+			}
+
+			[[nodiscard]] constexpr bool space(char a_ch) noexcept
+			{
+				return a_ch == ' ';
+			}
+
+			[[nodiscard]] constexpr bool wildcard(char a_ch) noexcept
+			{
+				return a_ch == '?';
+			}
+		}
+
+		namespace rules
+		{
+			namespace detail
+			{
+				[[nodiscard]] constexpr std::byte hexacharacters_to_hexadecimal(char a_hi, char a_lo) noexcept
+				{
+					constexpr auto lut = []() noexcept {
+						std::array<std::uint8_t, (std::numeric_limits<unsigned char>::max)() + 1> a = {};
+
+						const auto iterate = [&](std::uint8_t a_iFirst, unsigned char a_cFirst, unsigned char a_cLast) noexcept {
+							for (; a_cFirst <= a_cLast; ++a_cFirst, ++a_iFirst) {
+								a[a_cFirst] = a_iFirst;
+							}
+						};
+
+						iterate(0, '0', '9');
+						iterate(0xA, 'A', 'F');
+						iterate(0xa, 'a', 'f');
+
+						return a;
+					}();
+
+					return static_cast<std::byte>(
+						lut[static_cast<unsigned char>(a_hi)] * 0x10u +
+						lut[static_cast<unsigned char>(a_lo)]);
+				}
+			}
+
+			template <char HI, char LO>
+			struct Hexadecimal
+			{
+				[[nodiscard]] static constexpr bool match(std::byte a_byte) noexcept
+				{
+					constexpr auto expected = detail::hexacharacters_to_hexadecimal(HI, LO);
+					return a_byte == expected;
+				}
+			};
+
+			static_assert(Hexadecimal<'5', '7'>::match(std::byte{ 0x57 }));
+			static_assert(Hexadecimal<'6', '5'>::match(std::byte{ 0x65 }));
+			static_assert(Hexadecimal<'B', 'D'>::match(std::byte{ 0xBD }));
+			static_assert(Hexadecimal<'1', 'C'>::match(std::byte{ 0x1C }));
+			static_assert(Hexadecimal<'F', '2'>::match(std::byte{ 0xF2 }));
+			static_assert(Hexadecimal<'9', 'f'>::match(std::byte{ 0x9f }));
+
+			static_assert(!Hexadecimal<'D', '4'>::match(std::byte{ 0xF8 }));
+			static_assert(!Hexadecimal<'6', '7'>::match(std::byte{ 0xAA }));
+			static_assert(!Hexadecimal<'7', '8'>::match(std::byte{ 0xE3 }));
+			static_assert(!Hexadecimal<'6', 'E'>::match(std::byte{ 0x61 }));
+
+			struct Wildcard
+			{
+				[[nodiscard]] static constexpr bool match(std::byte) noexcept { return true; }
+			};
+
+			static_assert(Wildcard::match(std::byte{ 0xB9 }));
+			static_assert(Wildcard::match(std::byte{ 0x96 }));
+			static_assert(Wildcard::match(std::byte{ 0x35 }));
+			static_assert(Wildcard::match(std::byte{ 0xE4 }));
+
+			template <char, char>
+			void rule_for() noexcept;
+
+			template <char C1, char C2>
+			Hexadecimal<C1, C2> rule_for() noexcept
+				requires(characters::hexadecimal(C1) && characters::hexadecimal(C2));
+
+			template <char C1, char C2>
+			Wildcard rule_for() noexcept
+				requires(characters::wildcard(C1) && characters::wildcard(C2));
+		}
+
+		template <class... Rules>
+		struct PatternMatcher
+		{
+			static_assert(sizeof...(Rules) >= 1, "must provide at least 1 rule for the pattern matcher");
+
+			[[nodiscard]] constexpr bool match(std::span<const std::byte, sizeof...(Rules)> a_bytes) const noexcept
+			{
+				std::size_t i = 0;
+				return (Rules::match(a_bytes[i++]) && ...);
+			}
+
+			[[nodiscard]] bool match(std::uintptr_t a_address) const noexcept
+			{
+				return this->match(*reinterpret_cast<const std::byte(*)[sizeof...(Rules)]>(a_address));
+			}
+
+			void match_or_fail(std::uintptr_t a_address, std::source_location a_loc = std::source_location::current()) const noexcept
+			{
+				if (!this->match(a_address)) {
+					const auto version = Module::get().version();
+					stl::report_and_fail(
+						fmt::format(
+							"A pattern has failed to match.\n"
+							"This means the plugin is incompatible with the current version of the game ({}.{}.{}). "
+							"Head to the mod page of this plugin to see if an update is available."sv,
+							version[0],
+							version[1],
+							version[2]),
+						a_loc);
+				}
+			}
+		};
+
+		void consteval_error(const char* a_error);
+
+		template <stl::nttp::string S, class... Rules>
+		[[nodiscard]] constexpr auto do_make_pattern() noexcept
+		{
+			if constexpr (S.length() == 0) {
+				return PatternMatcher<Rules...>();
+			} else if constexpr (S.length() == 1) {
+				constexpr char c = S[0];
+				if constexpr (characters::hexadecimal(c) || characters::wildcard(c)) {
+					consteval_error("the given pattern has an unpaired rule (rules are required to be written in pairs of 2)");
+				} else {
+					consteval_error("the given pattern has trailing characters at the end (which is not allowed)");
+				}
+			} else {
+				using rule_t = decltype(rules::rule_for<S[0], S[1]>());
+				if constexpr (std::same_as<rule_t, void>) {
+					consteval_error("the given pattern failed to match any known rules");
+				} else {
+					if constexpr (S.length() <= 3) {
+						return do_make_pattern<S.template substr<2>(), Rules..., rule_t>();
+					} else if constexpr (characters::space(S[2])) {
+						return do_make_pattern<S.template substr<3>(), Rules..., rule_t>();
+					} else {
+						consteval_error("a space character is required to split byte patterns");
+					}
+				}
+			}
+		}
+
+		template <class... Bytes>
+		[[nodiscard]] consteval auto make_byte_array(Bytes... a_bytes) noexcept
+			-> std::array<std::byte, sizeof...(Bytes)>
+		{
+			static_assert((std::integral<Bytes> && ...), "all bytes must be an integral type");
+			return { static_cast<std::byte>(a_bytes)... };
+		}
+	}
+
+	template <stl::nttp::string S>
+	[[nodiscard]] constexpr auto make_pattern() noexcept
+	{
+		return detail::do_make_pattern<S>();
+	}
+
+	static_assert(make_pattern<"40 10 F2 ??">().match(
+		detail::make_byte_array(0x40, 0x10, 0xF2, 0x41)));
+	static_assert(make_pattern<"B8 D0 ?? ?? D4 6E">().match(
+		detail::make_byte_array(0xB8, 0xD0, 0x35, 0x2A, 0xD4, 0x6E)));
+}
+
+namespace std
+{
+	[[nodiscard]] inline std::string to_string(REL::Version a_version)
+	{
+		return a_version.string("."sv);
+	}
+
+#ifdef __cpp_lib_format
+	template <class CharT>
+	struct formatter<REL::Version, CharT> : formatter<std::string, CharT>
+	{
+		template <class FormatContext>
+		auto format(const REL::Version& a_version, FormatContext& a_ctx)
+		{
+			return formatter<std::string, CharT>::format(to_string(a_version), a_ctx);
+		}
+	};
+#endif
+}
+
+namespace fmt
+{
+	template <class CharT>
+	struct formatter<REL::Version, CharT> : formatter<std::string, CharT>
+	{
+		template <class FormatContext>
+		auto format(const REL::Version& a_version, FormatContext& a_ctx)
+		{
+			return formatter<std::string, CharT>::format(std::to_string(a_version), a_ctx);
+		}
 	};
 }
 
